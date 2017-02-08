afd57535b5f6c30e4b72de5da4993966335f9e11
Harin De Mel <demel.harin@gmail.com>
2017-02-07 21:20:07 -0500

    Delete daemon.py

--- daemon.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.system('sudo ifdown wlan0')
-os.system('sudo cp /home/pi/gpf/supportingfiles/interfaces.ap /etc/network/interfaces')
-os.system('sudo ifup wlan0')
-os.system('sudo service hostapd start')
-os.system('sudo service dnsmasq start')
-os.system('cd /home/pi/gpf')
-os.system('sudo python httpserver.py &')
